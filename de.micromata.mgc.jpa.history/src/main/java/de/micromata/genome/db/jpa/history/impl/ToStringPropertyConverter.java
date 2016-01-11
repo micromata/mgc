653fe36431702c7fed04a3b7d1521ed80bea15a4
Roger Kommer <r.kommer.extern@micromata.de>
2016-01-11 17:45:19 +0100

    Minor

--- de.micromata.mgc.jpa.history/src/main/java/de/micromata/genome/db/jpa/history/impl/ToStringPropertyConverter.java
+++ de.micromata.mgc.jpa.history/src/main/java/de/micromata/genome/db/jpa/history/impl/ToStringPropertyConverter.java
@@ -1,12 +1,7 @@
 package de.micromata.genome.db.jpa.history.impl;
 
-import java.util.Collections;
-import java.util.List;
-
 import org.apache.commons.lang.ObjectUtils;
 
-import de.micromata.genome.db.jpa.history.api.HistProp;
-import de.micromata.genome.jpa.IEmgr;
 import de.micromata.genome.jpa.metainf.ColumnMetadata;
 
 /**
@@ -19,18 +14,9 @@ public class ToStringPropertyConverter extends SimplePropertyConverter
 {
 
   @Override
-  public List<HistProp> convert(IEmgr<?> emgr, HistoryMetaInfo historyMetaInfo, Object entity, ColumnMetadata pd)
-  {
-    Object bo = super.convert(emgr, historyMetaInfo, entity, pd);
-    if (bo == null) {
-      return Collections.singletonList(new HistProp(null, String.class.getName(), null));
-    }
-    return Collections.singletonList(new HistProp(null, String.class.getName(), bo.toString()));
-  }
-
-  @Override
   protected String convertToString(Object value, ColumnMetadata pd)
   {
-    return ObjectUtils.toString(value);
+    String ret = ObjectUtils.toString(value);
+    return ret;
   }
 }
