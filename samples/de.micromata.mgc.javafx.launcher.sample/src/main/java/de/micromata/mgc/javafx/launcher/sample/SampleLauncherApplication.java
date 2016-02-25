565e54ecea9e433e1c93803bde3fcf341e6b38e1
Roger Kommer <r.kommer.extern@micromata.de>
2016-02-25 18:26:02 +0100

    minor add translation to samplelauncher

--- samples/de.micromata.mgc.javafx.launcher.sample/src/main/java/de/micromata/mgc/javafx/launcher/sample/SampleLauncherApplication.java
+++ samples/de.micromata.mgc.javafx.launcher.sample/src/main/java/de/micromata/mgc/javafx/launcher/sample/SampleLauncherApplication.java
@@ -1,5 +1,11 @@
 package de.micromata.mgc.javafx.launcher.sample;
 
+import de.micromata.genome.util.i18n.ChainedResourceBundleTranslationResolver;
+import de.micromata.genome.util.i18n.DefaultWarnI18NTranslationProvider;
+import de.micromata.genome.util.i18n.I18NTranslationProvider;
+import de.micromata.genome.util.i18n.I18NTranslationProviderImpl;
+import de.micromata.genome.util.i18n.I18NTranslations;
+import de.micromata.genome.util.i18n.PlaceholderTranslationProvider;
 import de.micromata.mgc.jettystarter.JettyConfigModel;
 import de.micromata.mgc.jettystarter.JettyServer;
 import de.micromata.mgc.jettystarter.MgcApplicationWithJettyApplication;
@@ -12,6 +18,14 @@ import de.micromata.mgc.jettystarter.sample.SampleJettyServer;
  */
 public class SampleLauncherApplication extends MgcApplicationWithJettyApplication<SampleLocalSettingsConfigModel>
 {
+  public SampleLauncherApplication()
+  {
+    super();
+    I18NTranslationProvider provider = new DefaultWarnI18NTranslationProvider(new PlaceholderTranslationProvider(
+        new I18NTranslationProviderImpl(I18NTranslations.systemDefaultLocaleProvider(),
+            new ChainedResourceBundleTranslationResolver("mgclauncher", "mgcapp", "mgcjetty"))));
+    setTranslateService(provider);
+  }
 
   @Override
   public void reInit()
