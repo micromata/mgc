fe7043a1bd0ef3710341be52ce65fdb41272b9d5
Roger Kommer <r.kommer.extern@micromata.de>
2016-04-19 15:35:07 +0200

    HSQLDB as file

--- de.micromata.mgc.common/src/main/java/de/micromata/genome/util/runtime/config/jdbc/FileHsqlJdbcProviderServiceImpl.java
+++ de.micromata.mgc.common/src/main/java/de/micromata/genome/util/runtime/config/jdbc/FileHsqlJdbcProviderServiceImpl.java
@@ -21,18 +21,18 @@ package de.micromata.genome.util.runtime.config.jdbc;
  * @author Roger Rene Kommer (r.kommer.extern@micromata.de)
  *
  */
-public class MemoryHsqlJdbcProviderServiceImpl extends AbstractJdbcProviderServiceImpl
+public class FileHsqlJdbcProviderServiceImpl extends AbstractJdbcProviderServiceImpl
 {
 
-  public MemoryHsqlJdbcProviderServiceImpl()
+  public FileHsqlJdbcProviderServiceImpl()
   {
-    super("Embedded Memory HSQLDB", "org.hsqldb.jdbcDriver");
+    super("Embedded File HSQLDB", "org.hsqldb.jdbcDriver");
   }
 
   @Override
   public String getSampleUrl(String appName)
   {
-    return "jdbc:hsqldb:file:" + appName + ";shutdown=true;hsqldb.default_table_type=memory";
+    return "jdbc:hsqldb:file:" + appName + ";shutdown=true";
   }
 
 }
