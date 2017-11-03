d3484569a6d34ca87d9f439c206e3da07ab47ecb
Hendrik Thole <h.thole@micromata.de>
2017-11-03 10:55:07 +0100

    GENOME-2044 - removes deprecated Genome StringUtils

--- de.micromata.mgc.common/src/main/java/de/micromata/genome/util/strings/MiscStringUtils.java
+++ de.micromata.mgc.common/src/main/java/de/micromata/genome/util/strings/MiscStringUtils.java
@@ -36,7 +36,7 @@ public class MiscStringUtils
   public static String cutLeft(String s, int maxLength)
   {
     if (s == null)
-      return "";
+      return StringUtils.EMPTY;
     if (s.length() <= maxLength)
       return s;
     return s.substring(s.length() - maxLength);
